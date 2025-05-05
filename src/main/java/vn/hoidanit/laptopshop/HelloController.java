5a2f78862f93ad298d102b33b22502777e4f0600
Nivetha <taquanga6dp@gmail.com>
2025-05-05 19:53:40 +0900

    second

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hello")
     public String index() {
-        return "Hello World from spring with quangDev";
+        return "Hello World from spring with quangDev hahaha";
+    }
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access this page ";
+    }
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin can access this page ";
     }
     
     
