86d62f5c5f02c15192ae64242d0a718ab4b43857
taquang <taquanga6dp@gmail.com>
2025-10-31 20:34:31 +0900

    config table cart name

--- src/main/java/vn/quangit/laptopshop/domain/Cart.java
+++ src/main/java/vn/quangit/laptopshop/domain/Cart.java
@@ -2,17 +2,11 @@ package vn.quangit.laptopshop.domain;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 
 @Entity
+@Table(name = "cart")
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
